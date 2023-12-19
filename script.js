2cc238f99b1e6b59fb6f261046f9ca289c9bf979
PATILYASHH <yashpatil575757@gmail.com>
2023-12-19 06:55:36 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const portfolioButton = document.querySelector('.port');
+  portfolioButton.addEventListener("click", function() {
+    window.location.href = "portfolio.html";
+  });
+
+  const profileButton = document.querySelector('.prof');  // Change from '.port' to '.prof'
+  profileButton.addEventListener("click", function() {
+    window.location.href = "profile.html";
+  });
+});
