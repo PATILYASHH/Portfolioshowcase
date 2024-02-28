97e57a87f1599c13c3c208497ed5071ac1c3e5f0
Yash patil <149749700+PATILYASHH@users.noreply.github.com>
2024-02-28 17:32:37 +0000

    the big upgrade

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.skills-container ul li').forEach((element) => {
-    element.addEventListener('mouseenter', () => {
-        element.classList.add('hovered');
-    });
-    element.addEventListener('mouseleave', () => {
-        element.classList.remove('hovered');
-    });
-});
